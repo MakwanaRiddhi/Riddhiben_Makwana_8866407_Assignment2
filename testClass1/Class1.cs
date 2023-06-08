7916af35b67799a614935813a6ce2ff1a7be711e
Riddhiben Makwana <rmakwana6407@conestogac.on.ca>
2023-06-08 16:02:23 -0400

    Add project files.

---
+++ testClass1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+
+namespace testClass1
+{
+    [TestFixture]
+    public class Class1
+    {
+        //	Only one (1) test for a valid equilateral triangle
+        [Test]
+        public void ValidEquilateralTriangle_Input47and47and47_OutputValidEquilateralTriangle()
+        {
+            //Arrange
+            int firstside = 47;
+            int secondside = 47;
+            int thirdside = 47;
+
+            string expected = "The triangle is valid and is an EQUILATERAL";
+
+            //Act
+           
+        }
+    }
+}
